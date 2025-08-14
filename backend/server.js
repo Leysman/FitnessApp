edb597ded98627057cee122f6a0df32d4646c9d6
Leysman <nahoryansky@gmail.com>
2025-08-14 18:52:23 +0300

    update backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -78,14 +78,7 @@ app.use(
 // app.use('/', mainRoute);
 
 // Server static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+app.get('/health', (_, res) => res.json({ ok: true }));
 
 const port = process.env.PORT || 4000;
 
